c6a4910c17d5f49f2ce935e0ecced6b4044450c4
gepitis <loukianos99@.hotmail.com>
2018-03-06 10:15:53 +0000

    First commit

--- dotaML.py
+++ dotaML.py
@@ -2,20 +2,19 @@ import dota2api,time,pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, accuracy_score
+from sklearn.metrics import classification_report, accuracy_score,confusion_matrix
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras import regularizers
-from sklearn.grid_search import GridSearchCV
-from sklearn.preprocessing import StandardScaler,PolynomialFeatures
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold,StratifiedKFold
 
 with open('matches','rb') as fp:
     matches=pickle.load(fp)
 player_list=['player' + str(i) for i in range(0,10)]
 X=pd.get_dummies(matches[player_list])
-#poly=PolynomialFeatures(2,interaction_only=True)
-#X.values=poly.fit_transform(X.values)
 y=matches['radiant_win'].astype(int)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=7)
 
@@ -23,30 +22,35 @@ rfc = RandomForestClassifier(n_estimators=10)
 rfc.fit(X_train, y_train)
 rfc_pred = rfc.predict(X_test)
 print(classification_report(y_test,rfc_pred))
-no_features=players.shape[1]
+no_features=X.shape[1]
+no_features=10
 def generate_model(no_features, l2_penalty=0.0,  type=1, dropout_rate=0.0):
         model = Sequential()
         if type==1:
-            model.add(Dense(round(no_features), input_dim=no_features, kernel_regularizer=regularizers.l2(l2_penalty), activation='relu')) # last layer should be linear so that we can have negative values as well
+            model.add(Dense(round(no_features), input_dim=X.shape[1], kernel_regularizer=regularizers.l2(l2_penalty), activation='relu')) # last layer should be linear so that we can have negative values as well
             model.add(Dropout(dropout_rate))
             model.add(Dense(round(no_features/2), kernel_regularizer=regularizers.l2(l2_penalty),  activation='relu')) # last layer should be linear so that we can have negative values as well
             model.add(Dropout(dropout_rate))   
-            model.add(Dense(round(no_features/4), kernel_regularizer=regularizers.l2(l2_penalty),  activation='relu')) # last layer should be linear so that we can have negative values as well
-            model.add(Dense(1, kernel_regularizer=regularizers.l2(l2_penalty),  activation='relu')) # last layer should be linear so that we can have negative values as well
+            model.add(Dense(round(no_features/2), kernel_regularizer=regularizers.l2(l2_penalty),  activation='relu')) # last layer should be linear so that we can have negative values as well
+            model.add(Dense(1, kernel_regularizer=regularizers.l2(l2_penalty),  activation='sigmoid')) # last layer should be linear so that we can have negative values as well
 
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy']) #mse for regression
         return model
-
-estimator=KerasClassifier(build_fn=generate_model, epochs=100, batch_size=100 , verbose=1)
+n_folds=10
+seed=7
+kfold = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=seed).split(X_train,y_train)
+estimator=KerasClassifier(build_fn=generate_model, epochs=10, batch_size=1000 , verbose=1)
 l2_penalty=[0]#,  0.0001,  0.001,  0.01,  0.1] # usual parameters to check for EEG data
 type=[1]#
-dropout_rate = [0.0]#, 0.2]#, 0.4, 0.6]
+dropout_rate = [0.0]#, 0.2, 0.4, 0.6]
 param_grid = dict(no_features=[no_features],l2_penalty=l2_penalty, type=type, dropout_rate=dropout_rate)
 grid = None;
-grid = GridSearchCV( estimator=estimator, param_grid=param_grid, cv=2 , scoring='accuracy')
-grid = grid.fit(X_train.values, y_train)
-tr_result=grid.predict(X_train.values)
-tst_result=grid.predict(X_test.values)
+grid = GridSearchCV( estimator=estimator, param_grid=param_grid, cv=kfold , scoring='accuracy')
+grid_fit = grid.fit(X_train.values, y_train)
+tr_result=grid_fit.predict(X_train.values)
+tst_result=grid_fit.predict(X_test.values)
+
+print(confusion_matrix(tst_result,y_test))
 print(classification_report(y_test,tst_result))
         
 #matches2=api.get_match_history(start_at_match_id=id-1)
--- scraperdota.py
+++ scraperdota.py
@@ -1,29 +1,76 @@
 import dota2api,time,pickle
+import pandas as pd
 
 api = dota2api.Initialise("2FCB78DAFC1178FA4981A64F78CE46D1")
 #mymatches1=api.get_match_history(account_id=76561198044313708 )
 
-def search(lobby_type,matches):
-    ret_matches=[]
-    for m in matches:
-        if m["lobby_type"]==lobby_type:
-            ret_matches.append(m)
 
-    return ret_matches
-
-matches=[]
+player_list=['player' + str(i) for i in range(0,10)]
+matches=pd.DataFrame(columns=player_list)
+matches['radiant_win']=None
+matches['match_id']=None
+matches
+skill=3#3<-very high skill
+count=0
 for i in range(0,100):
-    call=api.get_match_history(skill=3)
-    id = call['matches'][-1]['match_id']
-    matches=matches+call['matches']
-    matches=search(7,matches)
-    while call['num_results']>0:
-        call = api.get_match_history(skill=3,start_at_match_id=id-1)
-        if call['num_results']>0:
-            id = call['matches'][-1]['match_id']
-            matches=matches+search(7,call['matches'])
-            time.sleep(1)
-    time.sleep(300)
+    time.sleep(60)
+    print('iteration ' + str(i) + '\n')
+    job=0
+    while job==0:
+        try:
+            hist=api.get_match_history(skill=skill)
+            job=1;
+        except :
+            print("Api connection error")
+            time.sleep(20)
+
+    id = hist['matches'][-1]['match_id']
+    num_results=hist['num_results']
+    for j in range(0,num_results):
+        job=0
+        while job==0:
+            try:
+                time.sleep(2)
+                match=api.get_match_details(match_id=hist['matches'][j]['match_id'])
+                job=1
+            except:
+                print("Api connection error")
+                time.sleep(2)
+               
+        if match['game_mode']==22:
+            matches.loc[count,player_list]= [match['players'][z]['hero_id'] for z in range(0,10)]
+            matches.loc[count,['radiant_win','match_id']]=[match['radiant_win'],match['match_id']]            
+            count=count+1
+    
+    
+       
+    while hist['num_results']>0:
+        time.sleep(2)
+        job=0
+        while job==0:
+            try:
+                hist=api.get_match_history(skill=skill,start_at_match_id=id-1)
+                job=1;
+            except :
+                print("Api connection error")
+        if hist['num_results']>0:
+            id = hist['matches'][-1]['match_id']
+            num_results=hist['num_results']
+            for j in range(0,num_results):
+                 job=0
+                 while job==0:
+                     try:
+                         time.sleep(2)
+                         match=api.get_match_details(match_id=hist['matches'][j]['match_id'])
+                         job=1
+                     except:
+                         print("Api connection error")
+                         time.sleep(2)
+               
+                 if match['game_mode']==22:
+                     matches.loc[count,player_list]= [match['players'][z]['hero_id'] for z in range(0,10)]
+                     matches.loc[count,['radiant_win','match_id']]=[match['radiant_win'],match['match_id']]            
+                     count=count+1
 
 
 
@@ -38,3 +85,7 @@ with open('matches','wb') as fp:
 #matches2=api.get_match_history(start_at_match_id=id-1)
 
 #next((item for item in matches if item['lobby_type']==8))
+    
+#sqnum = [hist['matches'][z]['match_seq_num'] for z in range(0,100)]
+#hist_seq_num=api.get_match_history_by_seq_num(start_at_match_seq_num=min(sqnum))
+#sqnum_new = [hist_seq_num['matches'][z]['match_seq_num'] for z in range(0,100)]
